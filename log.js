3fad34f84e1071dc96bdf70df3852db0180eac75
gordon518 <luckysohu@sohu.com>
2021-04-09 07:22:28 +0800

    Change log4j

--- log.js
+++ log.js
@@ -3,14 +3,23 @@
 var log4js = require("log4js");
 
 log4js.configure({  
-    appenders: [  
-      { type: 'console' },
-      { type: 'datefile', filename: './logs/system.log',  pattern: '.yyyy-MM-dd',  alwaysIncludePattern: true, category: 'system' }
-    ],
-    levels: {
-        system: 'all',
-    },
-  });
+	appenders: {
+		console: {
+			type: 'console'
+		},
+		infoLogs: { 
+    		type: 'dateFile',
+      		filename: './logs/system.log',
+      		backups:5,  // 仅保留最新的五个日志文件
+      		pattern: ".yyyy-MM-dd", // 用于确定何时滚动日志的模式
+      		alwaysIncludePattern: true,
+      		compress: true
+		}
+	},
+	categories: {
+		default: { appenders: [ 'console', 'infoLogs' ], level: 'info' }
+	}
+});
   
 exports.getLog=function() {
 	var log = log4js.getLogger('system');
